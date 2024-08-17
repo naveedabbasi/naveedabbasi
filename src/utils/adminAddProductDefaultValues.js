a869f87c0abf354f2974e5c285c039b402217261
naveedabbasi <innoxentamo@gmail.com>
2024-08-17 20:21:17 +0500

    1

--- src/utils/adminAddProductDefaultValues.js
+++
@@ -1,8 +0,0 @@
-export const defaultValues = {
-	name: "",
-	imageURL: "",
-	price: 0,
-	category: "",
-	brand: "",
-	description: "",
-};
--- src/utils/uniqueValues.js
+++
@@ -1,8 +0,0 @@
-export const getUniqueValues = (products, type) => {
-	if (type === "category") {
-		return ["All", ...new Set(products.map((item) => item.category))];
-	}
-	if (type === "brand") {
-		return ["all", ...new Set(products.map((item) => item.brand))];
-	}
-};
